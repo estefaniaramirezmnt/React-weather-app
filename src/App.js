c7c7ab189230d9d2e976ff028cf0a9ebc3ef19b4
Estefanía <estefaniaramirezmnt@gmail.com>
2023-12-18 11:26:25 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+    return (
+    <div>
+        <p>
+            This project was created by 
+            <a href="https://niaramirez-portfolio.netlify.app/" target="_blank" rel="noreferrer"> Nia Ramírez </a>
+             and is
+            <a href="https://github.com/estefaniaramirezmnt/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+        </p>
+    </div>
+    )
+}
+
+export default Footer;
