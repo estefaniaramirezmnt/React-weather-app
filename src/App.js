475bfe45c8db09b0f27c66853f5d277cc0f91739
Estefanía <estefaniaramirezmnt@gmail.com>
2023-12-19 13:17:26 +0100

    Added boostrap and axios to dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <Footer />
+      <div className="container">
+        <h1>Weather app</h1>
+        <Footer />
+        <button className="btn btn-primary">Primary</button>
+      </div>
     </div>
   );
 }
