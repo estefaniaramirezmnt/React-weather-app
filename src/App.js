96f4921982cd4c4ea30846389a949a27730672ec
Estefanía <estefaniaramirezmnt@gmail.com>
2023-12-19 13:20:35 +0100

    Added style.css to a new folder for styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
+import "./style/style.css";
+import Weather from "./components/Weather";
 import Footer from "./components/Footer";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Weather app</h1>
+        <Weather />
         <Footer />
         <button className="btn btn-primary">Primary</button>
       </div>
---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Weather() {
+  return (
+    <div>
+      <h2>Hello from Weather</h2>
+    </div>
+  );
+}
+
+export default Weather;
