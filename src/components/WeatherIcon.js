c4b796e31b6f68816656820ec611ab502ee75406
Estefanía <estefaniaramirezmnt@gmail.com>
2023-12-22 12:11:54 +0100

    Changed fog icons

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -19,8 +19,8 @@ function WeatherIcon(props) {
     "thunderstorm-night": "icons/thunderstorms-night.svg",
     "snow-day": "icons/snowflake.svg",
     "snow-night": "icons/snowflake.svg",
-    "mist-day": "icons/mist.svg",
-    "mist-night": "icons/mist.svg",
+    "mist-day": "icons/fog-day.svg",
+    "mist-night": "icons/fog-night.svg",
   };
 
   return (
