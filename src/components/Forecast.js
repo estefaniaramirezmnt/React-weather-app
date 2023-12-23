0ee926a0b0f944c641fb066e78f48811890db98f
Estefanía <estefaniaramirezmnt@gmail.com>
2023-12-23 11:40:06 +0100

    Added useEffect to load search city to forecast

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ForecastDay from "./ForecastDay";
 import "../style/Forecast.css";
 
 function Forecast(props) {
-  // console.log(props);
   const [loaded, setLoaded] = useState(false);
   const [forecastData, setForecastData] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
+
+
   function handleResponse(response) {
     setForecastData(response.data.daily);
-    console.log(response.data.daily);
     setLoaded(true);
   }
 
@@ -28,16 +31,10 @@ function Forecast(props) {
             return null;
           }
         })}
-        {/* <ForecastDay data={forecastData[2]} />
-        <ForecastDay data={forecastData[3]} />
-        <ForecastDay data={forecastData[4]} />
-        <ForecastDay data={forecastData[5]} />
-        <ForecastDay data={forecastData[6]} /> */}
       </div>
     );
   } else {
     let apiKey = "4efbbf43t600f8b07428238a0a4o0852";
-    // let city = props.city;
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
